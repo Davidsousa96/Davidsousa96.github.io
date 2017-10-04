b14ddb64bf01c41eacd27593165554a83efd9283
David Sousa <yagodavid123@gmail.com>
2017-10-04 09:45:14 -0300

    New Design

--- scripts/article.js
+++ scripts/article.js
@@ -34,11 +34,6 @@ const newModal = function(project){
     htmlStruct += '                <p>'+ project.description+'<p>';
     htmlStruct += '                <p><a href="'+project.links.itch+'"><i class="btnLink fa fa-gamepad"></i></a><p></p>';
     htmlStruct += '        </div>';
-    /*
-    htmlStruct += '        <div class="modal-footer">';
-    htmlStruct += '          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>';
-    htmlStruct += '        </div>';
-    */
     htmlStruct += '      </div>';
     htmlStruct += '    </div>';
     htmlStruct += '  </div>';
@@ -53,56 +48,13 @@ const newArticle = function(size, project){
     htmlStruct += '<article class="project -biggest col-md-'+size+' fg-link-color-light" style="background-image: url('+project.image+')" data-toggle="modal" data-target="#'+titleLink+'">';
     htmlStruct += '    <div class="description hidden-sm-down">';
     htmlStruct += '        <time class="datetime" datetime="27 fev 2017"></time>';
-    /*
-    htmlStruct += '        <div class="seemore" data-toggle="modal" data-target="#'+titleLink+'">';
-    htmlStruct += '            <i class="fa fa-plus"></i>';
-    htmlStruct += '        </div>';
-    */
     htmlStruct += '        <h3 class="title">'+project.title+'</h3>';
     htmlStruct += '    </div>';
-
-    
     htmlStruct += '    <div class="mobile-description hidden-md-up"  data-toggle="modal" data-target="#'+titleLink+'">';
     htmlStruct += '        <time class="datetime" datetime="27 fev 2017"></time>';
-    /*
-    htmlStruct += '        <div class="seemore" data-toggle="modal" data-target="#'+titleLink+'">';
-    htmlStruct += '            <i class="fa fa-plus"></i>';
-    htmlStruct += '        </div>';
-    */
     htmlStruct += '        <h3 class="title">'+project.title+'</h3>';
     htmlStruct += '    </div>';
     htmlStruct += '</article>';
     htmlStruct += '<!-- / project -->';
     return htmlStruct;
 }
-
-/*
-modal 
-<!-- -->
-<div class="modal fade" id="project-0" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Game Developed</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-            <img src="http://lorempixel.com/500/200" class="img-responsive col-md-12">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-            took a galley of type and scrambled it to make a type specimen book. It has survived 
-            not only five centuries, but also the leap into electronic typesetting, remaining 
-            essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-            sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-            software like Aldus PageMaker including versions of Lorem Ipsum
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>
-<!-- / -->
-*/
